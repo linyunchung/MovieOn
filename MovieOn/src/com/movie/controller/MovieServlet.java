41046ef0153e5f5ffa4c1b163e58309f7317ec1d
Bruce Wu <rs665507@gmail.com.tw>
2021-08-16 02:31:12 +0800

    我放完整的專案試看看  裡面有一隻context.xml要放到servers裡的tomcat資料夾裡,那個有設定連線池的東西

--- MovieOn/src/com/movie/controller/MovieServlet.java
+++ MovieOn/src/com/movie/controller/MovieServlet.java
@@ -34,7 +34,7 @@ public class MovieServlet extends HttpServlet {
 				/***************************1.接收請求參數 - 輸入格式的錯誤處理**********************/
 				String str = req.getParameter("movieId");
 				if (str == null || (str.trim()).length() == 0) {
-					errorMsgs.add("請輸入員工編號");
+					errorMsgs.add("請輸入電影編號");
 				}
 				// Send the use back to the form, if there were errors
 				if (!errorMsgs.isEmpty()) {
@@ -48,7 +48,7 @@ public class MovieServlet extends HttpServlet {
 				try {
 					movieId = new Integer(str);
 				} catch (Exception e) {
-					errorMsgs.add("員工編號格式不正確");
+					errorMsgs.add("電影編號格式不正確");
 				}
 				// Send the use back to the form, if there were errors
 				if (!errorMsgs.isEmpty()) {
